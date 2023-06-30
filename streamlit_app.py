f61d179ec65f06f17d93b821888047f0b51c84fd
Hicham Kerkri <43246193+HichamISIMA@users.noreply.github.com>
2023-07-01 01:24:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
-from embedchain import App
 import openai
 
 
